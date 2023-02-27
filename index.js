ed3c85656d9b696fc1f31b8190b04487c8e73679
riclop335 <riclop335@gmail.com>
2023-02-26 21:55:24 -0500

    Subida inicial

---
+++ index.js
@@ -0,0 +1,24 @@
+
+function cambiolet()
+{
+    let titulo = document.getElementById('java1').innerHTML = 'DESARROLLO DE SOFTWARE 2';
+}
+
+function cambiofony()
+{
+    let fondoy = document.getElementsByTagName("body")[0];
+    fondoy.style.backgroundColor='yellow';
+}
+
+
+function cambiofonb()
+{
+    let fondob = document.getElementsByTagName("body")[0];
+    fondob.style.backgroundColor='blue';
+}
+
+function cambiofonr()
+{
+    let fondor = document.getElementsByTagName("body")[0];
+    fondor.style.backgroundColor='red';
+}
